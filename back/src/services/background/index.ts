74c53463e0056492e3c4842b33d768a3835f6a8f
JiveOff <antoine@jiveoff.fr>
2024-05-10 23:46:29 +0200

    chore: background

--- back/src/services/background/index.ts
+++ back/src/services/background/index.ts
@@ -1,6 +1,3 @@
-import Elysia from "elysia";
-import { injectStorePlugin } from "../../middlewares/inject-store";
-
-export const BackgroundService = new Elysia()
-  .use(injectStorePlugin)
-  .get("/", () => "Hello from background service");
+export class BackgroundService {
+  constructor() {}
+}
