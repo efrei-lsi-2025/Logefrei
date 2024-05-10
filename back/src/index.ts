46a64d6c0afbb3d5fbe90b37e7e10fcbc7b6b7b2
JiveOff <antoine@jiveoff.fr>
2024-05-10 21:11:02 +0200

    chore: fix production

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,21 +1,7 @@
 import Elysia from "elysia";
-import { parseArgs } from "util";
-
-const {
-  values: { service },
-} = parseArgs({
-  args: Bun.argv,
-  options: {
-    service: {
-      type: "string",
-    },
-  },
-  strict: true,
-  allowPositionals: true,
-});
 
 const main = () => {
-  console.log(service);
+  console.log(process.env.SERVICE);
 
   const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 };
