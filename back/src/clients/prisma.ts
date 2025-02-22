0525e2a97784d0ef580da808006288378e76b50a
JiveOff <antoine@jiveoff.fr>
2025-02-22 16:36:38 +0100

    chore: verbose back

--- back/src/clients/prisma.ts
+++ back/src/clients/prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-    log: [
-        {
-            emit: 'event',
-            level: 'query'
-        }
-    ]
+    log: ['query', 'info', 'warn', 'error']
 });
 
 export default prisma;
